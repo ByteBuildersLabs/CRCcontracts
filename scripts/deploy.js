3819c12217ef7d89f6d2c31398574a2c9801afee
RolandoDrRobot <rolando.arguello.email@gmail.com>
2024-10-20 12:37:48 -0600

    Update contracts addresses for chainlink and infura

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 async function main() {
   // Configuración de las direcciones y parámetros
-  const initialOwner = "0xYourOwnerAddress";  // Sustituye con la dirección del propietario inicial
-  const ethUsdPriceFeedAddress = "0xYourChainlinkPriceFeedAddress"; // Direccion del feed de precios de Chainlink (por ejemplo en Goerli)
+  const initialOwner = "0xf89bf5Dad545572362d119B0E5b3af4cF08f1C3b";  // Sustituye con la dirección del propietario inicial
+  const ethUsdPriceFeedAddress = "0x694AA1769357215DE4FAC081bf1f309aDC325306"; // Direccion del feed de precios de Chainlink (por ejemplo en Goerli)
   const usdToColonRate = 512;  // Ejemplo de tasa de conversión USD a Colones
 
   // Obtener la fábrica del contrato
